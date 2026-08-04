501cef5a0ee08175166dc662142dc78f0d43358c
shivang sahu <sahushivang749@gmail.com>
2026-08-04 22:48:03 +0530

    class 6 added

--- Class6.cs
+++ Class6.cs
@@ -8,5 +8,19 @@ namespace ConsoleApp3
 {
     internal class Class6
     {
+        static void Main7(string[] args)
+{
+            Console.Write("Enter Number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int fact = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+
+            Console.WriteLine("Factorial = " + fact);
+        }
     }
 }
