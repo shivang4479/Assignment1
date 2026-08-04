5d9ef14a8487c6b4cfd9da0815a80cf5e1ab3575
shivang sahu <sahushivang749@gmail.com>
2026-08-04 21:06:43 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class1
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("enter first no.");
+            int num1 =int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second no.");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.Write($"Sum={num1 + num2}");
+            Console.Write($"Diff={num1 - num2}");
+            Console.Write($"Pro={num1 * num2}");
+            Console.Write($"Qout={num1 / num2}");
+            Console.Write($"Rem={num1 % num2}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Console.WriteLine("enter name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("enter roll no");
+            int roll=int.Parse (Console.ReadLine());
+
+            Console.WriteLine("enter marks");
+            double mark = double.Parse(Console.ReadLine());
+
+            Console.Write($"your name: {name}");
+            Console.Write($"your roll no.:{roll}");
+            Console.Write($"your marks: {mark}");
+        }
+    }
+}
