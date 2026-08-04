b528116940880a9ca84980ae9acb6ed1248d3804
shivang sahu <sahushivang749@gmail.com>
2026-08-04 22:48:39 +0530

    class 7 added

--- Class7.cs
+++ Class7.cs
@@ -8,5 +8,27 @@ namespace ConsoleApp3
 {
     internal class Class7
     {
+        static void Main8(string[] args)
+        {          
+            Console.Write("Enter Number: ");
+            int num = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 2)
+            {
+                Console.WriteLine("Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+        }
     }
 }
