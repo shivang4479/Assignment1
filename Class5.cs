c2dc360b0ef6b21e8c2bcfd680de3d6513505b35
shivang sahu <sahushivang749@gmail.com>
2026-08-04 21:45:17 +0530

    class 5 added

---
+++ Class5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class5
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Even no.");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+            Console.Write("odd no.");
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+
+
+        }
+    }
+}
