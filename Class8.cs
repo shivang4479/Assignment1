3a4b20f74adf4496cd859c70f9ce64d02faafc8d
shivang sahu <sahushivang749@gmail.com>
2026-08-04 22:03:58 +0530

    class 8 added

---
+++ Class8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Class8
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int original = num;
+            int reverse = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                reverse = reverse * 10 + digit;
+                num = num / 10;
+            }
+
+            if (original == reverse)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not Palindrome");
+            }
+        }
+    }
+}
