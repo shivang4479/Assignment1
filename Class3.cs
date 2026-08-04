c70816220c70c4a49d8fc73e5c06706ab0ef0a6e
shivang sahu <sahushivang749@gmail.com>
2026-08-04 21:24:50 +0530

    code added in class 3

--- Class3.cs
+++ Class3.cs
@@ -11,6 +11,33 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
 
+            Console.Write("Enter Marks: ");
+            int marks = int.Parse(Console.ReadLine());
+
+            if (marks >= 91)
+            {
+                Console.WriteLine("Grade: O");
+            }
+            else if (marks >= 81)
+            {
+                Console.WriteLine("Grade: A+");
+            }
+            else if (marks >= 71)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (marks >= 61)
+            {
+                Console.WriteLine("Grade: B+");
+            }
+            else if (marks >= 51)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
         }
     }
 }
